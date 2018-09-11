f9f5f1d566cede1d6d6104762a95eea70ad938cc
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-09-11 20:56:13 -0300

    Controller senha única explicado

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,117 +1,56 @@
 class UsuariosController < ApplicationController
-  before_action :set_usuario, only: [:show, :edit, :update, :destroy]
-
-
-  # GET /usuarios
-  # GET /usuarios.json
-  def index
-
-    @usuarios = Usuario.all
-
-=begin
-
-    hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
-    @request_token  = OAuth::RequestToken.from_hash(@@consumer, hash)
-
-    @access_token = @request_token.get_access_token(@request_token, oauth_verifier: params[:oauth_verifier])
-=end
-
-=begin
-    key = "icb_oauth"
-    secret = SECRETOAUTH
-    @callback_id = "&callback_id=1"
-
-    @consumer = OAuth::Consumer.new(key, secret, {
-                                           :site               => "https://uspdigital.usp.br",
-                                           :scheme             => :header,
-                                           :http_method        => :post,
-                                           :request_token_path => "/wsusuario/oauth/request_token",
-                                           :access_token_path  => "/wsusuario/oauth/access_token",
-                                           :authorize_path     => "/wsusuario/oauth/authorize",
-                                           :resource           => "/wusuario/oauth/usuariousp"
-                                       })
-
-
-
-    @result =  @consumer.request(:post, "https://uspdigital.usp.br/wusuario/oauth/usuariousp", params[:oauth_token])
-
-    #@request_token = @consumer.get_request_token
-
-    print session[:oauth_token]
-
-    print @request_token.to_yaml
-
-    print @result.to_yaml
-=end
-
-=begin
-    @result =  @acesso.request(:post, "wusuario/oauth/usuariousp/" , session[:oauth_token])
-
-    print @result.to_yaml
-=end
-
-=begin
-    key = "icb_oauth"
-    secret = SECRETOAUTH
-
-
-    @consumer = OAuth::Consumer.new(key, secret, {
-                                           :site               => "https://uspdigital.usp.br",
-                                           :scheme             => :header,
-                                           :http_method        => :post,
-                                           :request_token_path => "/wsusuario/oauth/request_token",
-                                           :access_token_path  => "/wsusuario/oauth/access_token",
-                                           :authorize_path     => "/wsusuario/oauth/authorize",
-                                           :resource           => "/wusuario/oauth/usuariousp"
-                                       })
-
-
-
-    @result =  @consumer.request(:post, "https://uspdigital.usp.br/wusuario/oauth/usuariousp", session[:oauth_token])
-
-    @access_token = @request_token
-
-    print @access_token
-
-    @userusp = @access_token.gets('/wusuario/oauth/usuariousp')
-
-    print @userusp.to_yaml
-=end
-
-    key = "icb_oauth"
-    secret = SECRETOAUTH
-
-
-    @consumer = OAuth::Consumer.new(key, secret, {
-                                           :site               => "https://uspdigital.usp.br",
-                                           :scheme             => :header,
-                                           :http_method        => :post,
-                                           :request_token_path => "/wsusuario/oauth/request_token",
-                                           :access_token_path  => "/wsusuario/oauth/access_token",
-                                           :authorize_path     => "/wsusuario/oauth/authorize",
-                                           :resource           => "/wusuario/oauth/usuariousp"
-                                       })
-
-
-    @request_token = @consumer.get_request_token
-    @access_token = @request_token.get_access_token
-    @result = @access_token.get('/wusuario/oauth/usuariousp')
-
-
-    print "Retornooooo"
-    print @result.to_yaml
-
-  end
-
 
   def login
 
-    @callback_id = "&callback_id=1"
+    # Parte conceitual: http://oauthbible.com/
+    # A USP usa o OAuth 1.0a (Three Legged)
 
-    key = "icb_oauth"
+    # 1. Gera objeto consumidor
+    @consumer = gera_consumidor()
+
+    # 2. Faz a primeira requisição, pegando request token/secret, que serão futuramente trocados por um access_token/secret
+    @request_token = @consumer.get_request_token
+
+    # 3. Salva o par de request token/secret gerados na sessão, pois serão usados na troca
+    session[:token] = @request_token.token
+    session[:token_secret] = @request_token.secret
+
+    # 4. Envia usuário para URL de autorização 
+    redirect_to @request_token.authorize_url + @callback_id
+
+  end
+
+  def callback
+
+    # 5. Gera um objeto consumidor - na verdade não precisaria, poderia usar o mesmo, but sorry, I do not know Ruby...
+    @consumer = gera_consumidor()
+
+    # 6. Gera um hash (tipo um dicionário em python com chave/valor) com os requests token que estão na sessão (passo 3)
+    hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
+
+    # 7. Constrói o objeto @request_token para fazermos a troca
+    @request_token  = OAuth::RequestToken.from_hash(@consumer, hash)
+
+    # 8. Requisita o access token, passando oauth_verifier retornado via get (Three Legged)
+    @access_token = @request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
+
+    # 9. Com posse do access token, faça uma requisição POST
+    json_response = @access_token.post("https://uspdigital.usp.br/wsusuario/oauth/usuariousp");
+    @data = JSON.parse(json_response.body)
+
+    # 10. Com os dados, seja feliz!! (e claro, salve os dados localmente, faça login do fulano(a), etc etc)
+    print @data
+    
+
+  end
+
+  def gera_consumidor
+
+    @callback_id = "&callback_id=164"
+    key = "fflch_sti"
     secret = SECRETOAUTH
 
-    @consumer = OAuth::Consumer.new(key, secret, {
+    return OAuth::Consumer.new(key, secret, {
                                            :site               => "https://uspdigital.usp.br",
                                            :scheme             => :header,
                                            :http_method        => :post,
@@ -119,80 +58,7 @@ class UsuariosController < ApplicationController
                                            :access_token_path  => "/wsusuario/oauth/access_token",
                                            :authorize_path     => "/wsusuario/oauth/authorize"
                                        })
-
-
-    @request_token = @consumer.get_request_token
-
-    session[:token] = @request_token.token
-    session[:token_secret] = @request_token.secret
-    redirect_to @request_token.authorize_url + @callback_id
-
   end
 
-  # GET /usuarios/1
-  # GET /usuarios/1.json
-  def show
-  end
 
-  # GET /usuarios/new
-  def new
-    @usuario = Usuario.new
-  end
-
-  # GET /usuarios/1/edit
-  def edit
-  end
-
-  # POST /usuarios
-  # POST /usuarios.json
-  def create
-    @usuario = Usuario.new(usuario_params)
-
-    respond_to do |format|
-      if @usuario.save
-        format.html { redirect_to @usuario, notice: 'Usuario was successfully created.' }
-        format.json { render :show, status: :created, location: @usuario }
-      else
-        format.html { render :new }
-        format.json { render json: @usuario.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /usuarios/1
-  # PATCH/PUT /usuarios/1.json
-  def update
-    respond_to do |format|
-      if @usuario.update(usuario_params)
-        format.html { redirect_to @usuario, notice: 'Usuario was successfully updated.' }
-        format.json { render :show, status: :ok, location: @usuario }
-      else
-        format.html { render :edit }
-        format.json { render json: @usuario.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /usuarios/1
-  # DELETE /usuarios/1.json
-  def destroy
-    @usuario.destroy
-    respond_to do |format|
-      format.html { redirect_to usuarios_url, notice: 'Usuario was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-
-    def set_usuario
-      @usuario = Usuario.find(params[:id])
-
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def usuario_params
-      params.require(:usuario).permit(:nome)
-    end
 end
